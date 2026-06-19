3d76efb8c7193981043d73c2f841c4aa6748b842
Daniël Koek <contact@danielkoek.net>
2026-06-19 12:01:52 +0100

    update health probe

--- src/Teams.Notifications.Api/Program.cs
+++ src/Teams.Notifications.Api/Program.cs
@@ -209,7 +209,7 @@ builder.RegisterOpenTelemetry(appPathPrefix).Build();
 
 var app = builder.Build();
 await app.Services.GetRequiredService<ICosmosMessageStore>().EnsureContainerIsProvisioned();
-app.MapHealthChecks("/health");
+app.MapHealthChecks(appPathPrefix + "/health");
 app.MapOpenApi(appPathPrefix + "/swagger/{documentName}/openapi.json");
 app.UseSwaggerUI(c =>
 {
