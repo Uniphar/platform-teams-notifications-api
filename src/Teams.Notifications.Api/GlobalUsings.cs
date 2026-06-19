e1ca8c39916fce225d3e93d716cf35b92dfc83c0
Daniël Koek <contact@danielkoek.net>
2026-06-19 14:17:51 +0100

    clean

--- src/Teams.Notifications.Api/GlobalUsings.cs
+++
@@ -1,8 +0,0 @@
-// Global using directives
-
-global using Azure.Extensions.AspNetCore.Configuration.Secrets;
-global using Azure.Messaging.ServiceBus;
-global using Azure.Messaging.ServiceBus.Administration;
-global using Azure.Security.KeyVault.Secrets;
-global using Microsoft.AspNetCore.Mvc.Filters;
-global using Teams.Notifications.Api.Commands;
